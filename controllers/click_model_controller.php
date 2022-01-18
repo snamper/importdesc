89043056ba5cbf77aa0852177d386fe3d3cf4df6
givanov95 <joroz95@gmail.com>
2022-01-18 14:23:38 +0200

    branch for eri

--- controllers/click_model_controller.php
+++ controllers/click_model_controller.php
@@ -5,37 +5,7 @@ include_once 'views/view.php';
 class element
 {
 	private $id;
-	private $priceType;
-	private $vehicleType;
-	private $make;
-	private $model;
-	private $generation;
-	private $serie;
-	private $trim;
-	private $equipment;
-	private $uitvoering;
-	private $brandstofSoort;
-	private $eersteToelating;
-	private $huidigeDatumBPM;
-	private $tenaamstellingNL;
-	private $referentie;
-	private $co2NEDC;
-	private $co2WLTP;
-	private $restwaardePercentage;
-	private $inkoopprijsNettoBTW;
-	private $afleverkosten;
-	private $opknapkostenEX;
-	private $transportBuitenland;
-	private $transportBinnenland;
-	private $taxatieKosten;
-	private $kostenTotaal;
-	private $fee;
-	private $verkoopprijsNetto;
-	private $btw;
-	private $restBPMIndicatief;
-	private $leges;
-	private $verkoopprijsMarge;
-
+	
 
 	public function __get($property)
 	{
@@ -51,21 +21,12 @@ class click_model extends view{
 
     public function __construct() {
 		// displayed page index
-		$obj = (object)[];
+		$obj = new element();
 		$this->base = $_SESSION['base'];
-		$cars = $this->base->getAllCars();
+		// $cars = $this->base->getAllCars();
 
-		if(isset($_POST['add_calculation'])) {
-			$this->base->insertCalculation($_POST);
-		}
 
-		if(isset($_POST['car_id_to_connect'])) {
-			$this->connectCalculationCar($_POST['calculation_id'], $_POST['car_id_to_connect']);
-			header("Location: /show_calculations");
-			exit;
-		}
-
-		if(isset($_SESSION['user']))parent::__construct('calculation_view.php');
+		if(isset($_SESSION['user']))parent::__construct('click_model_view.php');
 		else parent::__construct('login_view.php');
 
     }
