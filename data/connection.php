901d71f5d342f881c65a1d430ba4a0822ad10d0d
givanov95 <joroz95@gmail.com>
2022-02-16 09:57:04 +0200

    fix cached connection.php

--- data/connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-/* Database connection information */
-$gaSql['user']       = "deb64406n9_import1";
-$gaSql['password']   = "RbB38eSQ";
-$gaSql['db']         = "deb64406n9_import1";
-$gaSql['server']     = "localhost";
